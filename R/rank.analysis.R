50b20b624d9a83ab9c3593620f7788d59fd3f0b8
d-mathlete <daschoka@googlemail.com>
2017-03-03 08:50:51 +0100

    implemented tree of iderals approach.

--- R/rank.analysis.R
+++ R/rank.analysis.R
@@ -1,23 +1,26 @@
-rank_analysis=function(P,method="bfs",names="",print.level=0){
+rank_analysis=function(P,method="bfs",names="",only.results=T,verbose=F){
   #' @title Rank Analysis of networks
   #' @description  Performes a complete Rank analysis of a network.
-  #' Calculates Expected Rankings, (Mutual) Rank Probabilities and number of possible rankings.
+  #' Calculates Expected Rankings, (Relative) Rank Probabilities and number of possible rankings.
   #' Works best with small networks.
   #'
-  #' @param P a partial order as matrix object
-  #' @param method string indicating method to get all possible rankings. Default should not be changed
+  #' @param P matrix representing a partial ranking
+  #' @param method string indicating method to get all possible rankings. (Default should not be changed)
   #' @param names optional argument for names if P does not have row/column names
-  #' @param print.level should diagnostics be printed (1) or not (0). Defaults to 0
+  #' @param only.results logical. wether only results (default) or additionally the ideal tree and lattice should be returned
+  #' @param verbose logical. should diagnostics be printed. Defaults to F
   #' @details TODO
-  #' @return a list containing
-  #' \item{n.ext}{Number of Linear Extensions of P}
+  #' @return 
+  #' \item{n.ext}{Number of possible centrality rankings}
   #' \item{mse}{Array indicating equivalent nodes}
-  #' \item{rank.prob}{Matrix containing rank probabilities \code{rank.prob[i,k]} is the probability that i has rank k}
-  #' \item{mutual.rank}{Matrix containing mutual rank probabilities. \code{mutual.rank[i,j]} is the probability that i is ranked lower than j}
-  #' \item{expected.rank}{Expected Ranking of nodes in any centrality ranking}
-  #' \item{rank.spread}{???}
-  #' \item{g.lattice}{igraph object. The lattice of ideals}
+  #' \item{rank.prob}{Matrix containing rank probabilities: \code{rank.prob[i,k]} is the probability that i has rank k}
+  #' \item{mutual.rank}{Matrix containing relative rank probabilities: \code{mutual.rank[i,j]} is the probability that i is ranked lower than j}
+  #' \item{expected.rank}{Expected ranks of nodes in any centrality ranking}
+  #' \item{rank.spread}{Variance of the ranking probabilities}
+  #' \item{lattice}{igraph object. The lattice of ideals (if only.results=F)}
+  #' \item{tree}{igraph object. The tree of ideals (if only.results=F)}
   #' @references \insertRef{ddd-elirp-06}{netrankr}
+  #' @seealso [approx_rank_mutual], [approx_rank_expected]
   #' @examples
   #' ###TODO
   #' @export
@@ -29,7 +32,7 @@ rank_analysis=function(P,method="bfs",names="",print.level=0){
   }
   n.full <- nrow(P)
   P.full <- P
-  ###############################Equivalence
+  # Equivalence classes ------------------------------------------------
   MSE=which((P+t(P))==2,arr.ind=T)
   if(length(MSE)>=1){
     MSE<-t(apply(MSE,1,sort))
@@ -48,52 +51,78 @@ rank_analysis=function(P,method="bfs",names="",print.level=0){
   names <- rownames(P)
   #number of Elements
   m <- length(names)
-  ###############################LATTICEOFIDEALS
-  #calculate lattice of ideals
-  g.ideals=.lattice_of_ideals(P)
-  if(print.level==1){
-    print("lattice done")
-    print(c(igraph::vcount(g.ideals),igraph::ecount(g.ideals)))
+  # LATTICEOFIDEALS ----------------------------------------------------
+  n<-nrow(P)
+  topo.order<-P %>% 
+    igraph::graph_from_adjacency_matrix("directed") %>% 
+    igraph::topological.sort() %>% 
+    as.vector()
+  
+  tree<-.tree_of_ideals(P,topo.order)
+  if(verbose==T){
+    print("tree of ideals built")
+  }
+  g.ideals<-.build_lattice(tree)
+  if(verbose==T){
+    print("lattice of ideals built")
+  }
+  ideals<-.ideal_listing(P,topo.order)
+  ideals.integer<-sapply(ideals,function(x){strsplit(x," ") %>% unlist %>% as.integer},USE.NAMES = F)
+  ideals.integer<-lapply(ideals.integer,function(x){topo.order[x]})
+  if(verbose==T){
+    print("ideals listed")
+  }
+  V(g.ideals)$name=ideals
+  V(tree$graph)$name=ideals
+  el=get.edgelist(g.ideals,names = F)
+  E(g.ideals)$label=apply(el,1,function(x){a<-ideals.integer[[x[1]]];b<-ideals.integer[[x[2]]];union(setdiff(a,b),setdiff(b,a))})
+  
+  if(verbose==T){
+    print("lattice determined")
+    print(paste("No of ideals:",igraph::vcount(g.ideals)))
   }
   E(g.ideals)$color="grey"
   #number of ideals
   n=igraph::vcount(g.ideals)
-
-  ###############################TOPDOWN
+  
+  #TOPDOWN BFS ---------------------------------------------------------
+  #find full ideal
+  ideal.sink<-which.max(nchar(V(g.ideals)$name))
+  
   visited<<-rep(0,n)
   lef.g<<-rep(0,n)
-  e<-.assign_top_down(1,g.ideals)
+  e<-.assign_top_down(1,g.ideals,ideal.sink)
   lef<-lef.g
   rm(list=c("lef.g","visited"),pos = ".GlobalEnv")
-  if(print.level==1){
-    print("Top Down done")
-    print(c(e))
+  if(verbose==T){
+    print("Top Down BFS finished")
+    print(paste("Number of possible centrality rankings:",e))
   }
-  ###############################BottomUp
+  #BottomUp---------------------------------------------------------
   visited<<-rep(0,n)
   lei<-.assign_bottom_up(g.ideals,m,method)
   rm(visited,pos = ".GlobalEnv")
-  if(print.level==1){
-    print("Bottom Up done")
+  if(verbose==T){
+    print("Bottom Up BFS finished")
   }
-  ###############################RankProb
+  #RankProb---------------------------------------------------------
   rp.g<<-matrix(0,m,m)
   visited<<-rep(0,n)
-  .compute_rank_prob(g.ideals,1,1,lei,lef,e)#g,v,h,lei,lef,e
+  .compute_rank_prob(g.ideals,1,1,lei,lef,e,ideal.sink)
   rp=rp.g
   rm(list=c("visited","rp.g"),pos = ".GlobalEnv")
-  if(print.level==1){
-    print("Rank Prob done")
+  if(verbose==T){
+    print("Rank Probabilites calculated")
   }
-  ###############################MutualRankProb
+  #MutualRankProb----------------------------------------------------
   visited<<-rep(0,n)
   visitedElement<<-rep(0,n)
   mrp.g<<-matrix(0,m,m)
-  .compute_mutual_rank_prob(g.ideals,1,1,lei,lef,e,m)
+  .compute_mutual_rank_prob(g.ideals,1,1,lei,lef,e,m,ideal.sink)
   mrp=mrp.g
   rm(list=c("visited","visitedElement","mrp.g"),pos = ".GlobalEnv")
-  if(print.level==1){
-    print("Mutual Rank done")
+  if(verbose==T){
+    print("Mutual Rank Probabilities calculated")
   }
   ###############################END
   expected=rp%*%1:m
@@ -117,35 +146,181 @@ rank_analysis=function(P,method="bfs",names="",print.level=0){
     }
     else if(length(idx)==1){
       rp.full[idx,] <- rp[i,]
-
+      
       mrp.full[idx,] <- mrp[i,MSE]
       expected.full[idx] <- expected[i]
       rank.spread.full[idx] <- rank.spread[i]
     }
   }
   ###############################
-  return(list(lin.ext=e,
-              names=rownames(P.full),
-              mse=MSE,
-              rank.prob=rp.full,
-              mutual.rank.prob=t(mrp.full),
-              expected.rank=expected.full,
-              rank.spread=sqrt(rank.spread.full),
-              g.lattice=g.ideals))
+  if(only.results){
+    return(list(lin.ext=e,
+                names=rownames(P.full),
+                mse=MSE,
+                rank.prob=rp.full,
+                mutual.rank.prob=t(mrp.full),
+                expected.rank=expected.full,
+                rank.spread=sqrt(rank.spread.full)))
+  } else{
+    return(list(lin.ext=e,
+                names=rownames(P.full),
+                mse=MSE,
+                rank.prob=rp.full,
+                mutual.rank.prob=t(mrp.full),
+                expected.rank=expected.full,
+                rank.spread=sqrt(rank.spread.full),
+                lattice=g.ideals,
+                tree=tree$graph))
+  }
+  ###############################
 }
-#############################################################################################HELPER
-.assign_top_down=function(v,g){
+#HELPER FUNCTIONS----------------------------------------------------------------
+.tree_of_ideals=function(P,topo.order){
+  P<-P[topo.order,topo.order]
+  n<-nrow(P)
+  max.id=n+1
+  topo.sort<-topo.order
+  
+  topo.order<-1:max.id
+  g<-igraph::make_empty_graph() %>% 
+    igraph::add_vertices(n+1,attr=list(name=topo.order)) %>% 
+    igraph::add_edges(c(topo.order[1],rep(topo.order[2:n],each=2),n+1))
+  for(i in 3:(n+1)){
+    v=topo.order[i]
+    v.prec=topo.order[i-1]
+    id.v=which(igraph::V(g)$name==v)
+    pd.check=which(igraph::V(g)$name==v.prec)[1]
+    children<-neighborhood(g,1,pd.check,mode="in",mindist = 1) %>% unlist()
+    focal=rep(id.v,length(children))
+    while(length(children)>=1){
+      child.name=igraph::V(g)$name[children[1]]
+      child.id=children[1]
+      target=focal[1]
+      children=children[-1]
+      focal=focal[-1]
+      if(P[child.name,pd.check]!=1){
+        max.id=max.id+1
+        g<-g %>% igraph::add_vertices(1,attr=list(name=child.name)) %>% 
+          igraph::add_edges(c(max.id,target))
+        tmp<-igraph::neighborhood(g,1,child.id,mode="in",mindist = 1) %>% unlist
+        if(length(tmp)>0){
+          children= c(tmp,children)
+          focal=c(rep(max.id,length(tmp)),focal)
+        }
+      }
+    }
+  }
+  return(list(graph=g,topo.sort=topo.sort))
+  
+}
+
+#####################################################################################
+.build_lattice=function(tree){
+  topo=tree$topo.sort
+  tree=tree$graph
+  el<-get.edgelist(tree,names=F)
+  n=max(igraph::V(tree)$name)-1
+  pred=vector("list",igraph::vcount(tree))
+  sink=which(igraph::V(tree)$name==(n+1))
+  pred[[sink]]=igraph::neighborhood(tree,1,sink,"in",1) %>% unlist
+  Ek=map(1:n,function(x){which(igraph::V(tree)$name==x)})
+  
+  for(k in n:1){
+    for(v in Ek[[k]]){
+      parent.v=igraph::neighborhood(tree,1,v,"out",1) %>% unlist
+      children.v=igraph::neighborhood(tree,1,v,"in",1) %>% unlist
+      v.prime<-pred[[parent.v]][1]
+      j=1
+      while(v.prime!=v){
+        children.v.prime=igraph::neighborhood(tree,1,v.prime,"in",1) %>% unlist
+        v.star=children.v.prime[1]
+        pred[[v]]=c(pred[[v]],v.star)
+        j=j+1
+        v.prime<-pred[[parent.v]][j]
+      }
+      pred[[v]]=c(pred[[v]],children.v)
+    }
+    if(length(el)>2){ #this is just a workaround for Rs stupidity to not recognize 1 row matrices
+      del.edges<-which(el[,1]%in%Ek[[k]])
+      tree<-tree %>% igraph::delete_edges(del.edges)
+      el<-el[-del.edges,]
+    } else{
+      del.edges<-1
+      tree<-tree %>% igraph::delete_edges(del.edges)
+      el<-el[-c(1,2)]
+    }
+  }
+  lattice=graph_from_adj_list(pred,mode="in")
+  return(lattice)
+}
+#####################################################################################
+.ideal_listing=function(P,topo.order){
+  n=nrow(P)
+  P<-P[topo.order,topo.order]
+  P.work<-P
+  I=1:n
+  i<-1
+  S<-list()
+  S[[1]]=sort(which(rowSums(P)==0),decreasing=T)
+  Code=numeric()
+  ideals=I %>% sort %>% paste(collapse=" ")
+  while(i>0){
+    if(length(S[[i]]!=0)){
+      x<-S[[i]][1]
+      # Flag<-"BF"
+      Code=c(Code,x)
+      I=setdiff(I,x)
+      S[[i]]<-setdiff(S[[i]],x)
+      new.ideal<-I %>% sort %>% paste(collapse=" ")
+      # print(new.ideal)
+      if(!new.ideal%in%ideals){
+        ideals=c(ideals,new.ideal)
+      }
+      P.tmp=P.work
+      P.tmp[,x]=0
+      S2=which(rowSums(P.work)!=0 & rowSums(P.tmp)==0)
+      P.work=P.tmp
+      S[[i+1]]=sort(c(S[[i]],S2),decreasing=T) #!!!!!!!!!!!!!
+      i=i+1
+    } 
+    else{
+      # if(Flag=="BT"){
+      # if(!is.unsorted(I)){
+      #   print(I)
+      # }
+      new.ideal<-I %>% sort %>% paste(collapse=" ")
+      # print(new.ideal)
+      if(!new.ideal%in%ideals){
+        ideals=c(ideals,new.ideal)
+      }
+      # }
+      x=Code[length(Code)]
+      Code=Code[-length(Code)]
+      I=c(I,x)
+      # Flag="BT"
+      P.work[,x]=P[,x]
+      i=i-1
+    }
+  }
+  idx<-which(ideals=="")
+  ideals=ideals[c(idx:1,(idx+1):length(ideals))]
+  return(ideals)
+}
+
+##################################################################################################
+
+.assign_top_down=function(v,g,top){
   visited[v]<<-1
   e=0
   nv=igraph::neighborhood(g,1,v,"out",1)[[1]]
   for(w in nv){
-    if(w==vcount(g)){
+    if(w==top){
       e=1
       lef.g[w]<<-e
     }
     else{
       if(visited[w]==0){
-        e=e+.assign_top_down(w,g)
+        e=e+.assign_top_down(w,g,top)
       }
       else{
         e=e+lef.g[w]
@@ -155,7 +330,7 @@ rank_analysis=function(P,method="bfs",names="",print.level=0){
   lef.g[v]<<-e
   return(e)
 }
-
+########################################################################################
 .assign_bottom_up=function(g,m,method){
   if(method=="matrix"){
     A=igraph::get.adjacency(g,sparse=F)
@@ -184,32 +359,32 @@ rank_analysis=function(P,method="bfs",names="",print.level=0){
       nv=igraph::neighborhood(g,1,v,"out",1)[[1]]
       for(w in nv){
         lei[w]=lei[w]+lei[v]
-
+        
         if(visited[w]==0 & inList[w]==0){
           L=.lappend(L,w)
           inList[w]=1
-
+          
         }
       }
     }
   }
   return(lei)
 }
-
-.compute_rank_prob=function(g,v,h,lei,lef,e){
+####################################################################################################
+.compute_rank_prob=function(g,v,h,lei,lef,e,top){
   visited[v]<<-1
   nv=igraph::neighborhood(g,1,v,"out",1)[[1]]
   for(w in nv){
     idx=igraph::get.edge.ids(g,c(v,w))
     x=igraph::get.edge.attribute(g,"label",idx)
     rp.g[x,h]<<-rp.g[x,h]+(lei[v]*lef[w])/e
-    if(w!=vcount(g) & visited[w]==0){
-      .compute_rank_prob(g,w,h+1,lei,lef,e)
+    if(w!=top & visited[w]==0){
+      .compute_rank_prob(g,w,h+1,lei,lef,e,top)
     }
   }
 }
-
-.compute_mutual_rank_prob=function(g,v,h,lei,lef,e,m){
+####################################################################################################
+.compute_mutual_rank_prob=function(g,v,h,lei,lef,e,m,top){
   visited[v]<<-1
   nv=neighborhood(g,1,v,"out",1)[[1]]
   for(w in nv){
@@ -220,71 +395,16 @@ rank_analysis=function(P,method="bfs",names="",print.level=0){
         mrp.g[x,y]<<-mrp.g[x,y]+(lei[v]*lef[w])/e
       }
     }
-    if(w!=igraph::vcount(g) & visited[w]==0){
+    if(w!=top & visited[w]==0){
       idx=igraph::get.edge.ids(g,c(v,w))
       x=igraph::get.edge.attribute(g,"label",idx)
       visitedElement[x]<<-1
-      .compute_mutual_rank_prob(g,w,h+1,lei,lef,e,m)
+      .compute_mutual_rank_prob(g,w,h+1,lei,lef,e,m,top)
       visitedElement[x]<<-0
     }
   }
 }
-
-.lattice_of_ideals=function(P){ #P_ij=1 i<j
-  n=nrow(P)
-  g=igraph::graph.empty()
-  g=igraph::add.vertices(g,1,attr=list(name="empty"))
-  L=list()
-  min.elem=which(colSums(P)==0)
-  min.elem=sort(min.elem)
-  #append minimum elements to L
-  L=sapply(unname(min.elem),function(x).lappend(L,x))
-  #add minimum elements to graph
-  g=igraph::add.vertices(g,length(min.elem),attr=list(name=as.character(min.elem)))
-  #add edges
-  add.edges=c(matrix(c(rep("empty",length(min.elem)), min.elem), 2, byrow = T))
-  g=igraph::add.edges(g,add.edges,attr=list(label=min.elem)) #added attr
-  P.del=P
-  rownames(P.del)=1:nrow(P)
-  colnames(P.del)=1:nrow(P)
-  while(length(L)>=1){
-    P.del=P
-    rownames(P.del)=1:nrow(P)
-    colnames(P.del)=1:nrow(P)
-    I=L[[1]]
-    L[[1]]<-NULL
-    idx.del=which(rownames(P.del)%in%I)
-    if(length(idx.del>0)){
-      P.del=P.del[-idx.del,-idx.del]
-    }
-    if(length(P.del)==1){
-      min.elem=setdiff(1:n,I)
-    }
-    else{
-      min.elem=as.integer(rownames(P.del)[unname(which(colSums(P.del)==0))])
-    }
-
-    for(x in min.elem){
-      Iprime=c(I,x)
-      Iprime=sort(Iprime)
-      inL=Position(function(y) identical(y, Iprime), L, nomatch = 0) > 0
-      if(!inL){
-        L=.lappend(L,Iprime)
-        v.name=paste(Iprime,collapse=" ")
-        L.v.name=paste(I,collapse=" ")
-        g<-igraph::add.vertices(g,1,attr=list(name=v.name))
-        g<-igraph::add.edges(g,c(L.v.name,v.name),attr=list(label=setdiff(Iprime,I))) #added label
-      }
-      else{
-        v.name=paste(Iprime,collapse=" ")
-        L.v.name=paste(I,collapse=" ")
-        g<-igraph::add.edges(g,c(L.v.name,v.name),attr=list(label=setdiff(Iprime,I))) #added label
-      }
-    }
-  }
-  return(g)
-}
-
+####################################################################################################
 .lappend <- function (lst, ...){
   lst <- c(lst, list(...))
   return(lst)
