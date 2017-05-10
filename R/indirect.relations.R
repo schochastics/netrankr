1df5150238c1d6fc97ba01201967b43f793c4e21
schochastics <david.schoch@uni-konstanz.de>
2017-05-10 16:59:40 +0200

    added more relations

--- R/indirect.relations.R
+++ R/indirect.relations.R
@@ -1,66 +1,64 @@
 #' @title Indirect relations of a network
-#' @description Derive indirect relations, for instance distances, for a given network. 
+#' @description Derive indirect relations like distances, for a given network. This function
+#' can be used to calculate a variety of centrality indices.
 #' @param g igraph object. The network for which relations should be derived
-#' @param relation string. giving the relation to be calculated. See Details for options.
-#' @param walk_length integer. maximum length of walks to be considered if relation="walks"
-#' @param gamma vector of weights if relation = "walks". See Details.
-#' @details The \emph{relation} parameter can be set to 
-#' \describe{
-#' \item{identity}{returns the adjacency matrix of the network.}
-#' \item{distances}{calculated geodesic distances between all pairs of nodes.}
-#' \item{dependencies}{returns dyadic dependencies 
+#' @param relation string. giving the relation to be calculated. One of "identity", 
+#' "distances", "dependencies" or "walks". See Details.
+#' @param FUN a weighting function. See Details.
+#' @param ... additional arguments passed to the function FUN.
+#' @details The \emph{relation} parameter has the following options.  
+#' 
+#' \emph{"identity"} returns the adjacency matrix of the network.  
+#' 
+#' \emph{"distances"} returns geodesic distances between all pairs of nodes.  
+#' 
+#' \emph{"dependencies"} returns dyadic dependencies 
 #' \deqn{\delta(u,s) = \sum_{t \in V} \frac{\sigma(s,t|u)}{\sigma(s,t)}}
 #' where \eqn{\sigma(s,t|u)} is the number of shortest paths from s to t that include u and
 #' \eqn{\sigma(s,t)} is the total number of shortest (s,t)-paths. The row sums of the resulting matrix 
-#' is equal to the betweenness scores.
-#'  }
-#' \item{walks}{returns weighted walk counts of the form}
-#' \deqn{\sum_{i=0}^k \gamma_i A^i.}
-#' k is set by the `walk_length` parameter and \eqn{\gamma} by the vector provided by `gamma`.
-#' }
-#' @return a matrix containing indirect relations.
+#' is equal to the betweenness scores.  
+#' 
+#' \emph{"walks"} returns walk counts between pairs of nodes, usually they are 
+#' weighted decreasingly in their lengths or other properties which can be done by adding
+#' a function in \code{FUN}.  
+#' 
+#' \emph{"resistance"} returns the resistance distance between all pairs of vertices.
+#'  
+#' The function \code{FUN} is used to further specify the indirect
+#' relation. see `?transform_relations` for predefined functions.
+#' 
+#' @return a matrix containing indirect relations in a network.
+#' @author David Schoch
 #' @examples
 #' 
 #' require(igraph)
 #' 
-#' g <- graph.empty(n=11,directed = FALSE)
-#' g <- add_edges(g,c(1,11,2,4,3,5,3,11,4,8,5,9,5,11,6,7,6,8,
-#'                    6,10,6,11,7,9,7,10,7,11,8,9,8,10,9,10))
-#' #same as distances(g,mode="all")
-#' dist <- indirect_relations(g,"distances") 
-#' dep  <- indirect_relations(g,"dependencies")
-#' 
-#' #rowSums of dep equals 2*betweenness(g)
-#' rowSums(dep)-2*betweenness(g)
-#' 
-#' #indirect realtion for subgraph_centrality
-#' walk  <- indirect_relations(g,"walks",gamma=factorial(1:10))
-#' plot(rowSums(walk),subgraph_centrality(g))
 #' @export
-indirect_relations <- function(g,relation="distances",
-                              walk_length=10,
-                              gamma=factorial(1:walk_length)){
-  if(relation=="distances"){
-    return(igraph::distances(g,mode = "all"))
-  }
-  else if(relation=="identity"){
-    return(igraph::get.adjacency(g,type="both",sparse=FALSE))
-  }
-  else if(relation=="dependencies"){
-    adj <- lapply(igraph::get.adjlist(g),function(x)x-1)
-    return(dependency(adj))
-  }
-  else if(relation=="walks"){
-    A <- igraph::get.adjacency(g)
-    tmp <- list()
-    B <- diag(1,igraph::vcount(g))
-    for(k in 1:walk_length){
-      B <- B%*%A
-      tmp[[k]] <- B
-    }
-    for(i in 1:walk_length){
-      tmp[[i]] <- tmp[[i]]*gamma[i]^(-1)
-    }
-    return(as.matrix(Reduce("+",tmp)))
-  }
+indirect_relations <- function(g,relation="geodesic",
+                               FUN=identity,...){
+  
+  if(relation=="geodesic"){
+    rel <- igraph::distances(g,mode = "all")
+    rel <- FUN(rel,...)
+  } else if(relation=="identity"){
+      rel <- igraph::get.adjacency(g,type="both",sparse=FALSE)
+      rel <- FUN(rel,...)
+      diag(rel) <- 0
+  } else if(relation=="dependencies"){
+      adj <- lapply(igraph::get.adjlist(g),function(x)x-1)
+      rel <- dependency(adj)
+  } else if(relation=="walks"){
+      eigen.decomp <- eigen(igraph::get.adjacency(g,type="both"))
+      lambda <- eigen.decomp$values
+      X <- eigen.decomp$vectors
+      rel <- X%*%diag(FUN(lambda,...))%*%t(X)
+  } else if(relation=="resistance"){
+    L <- igraph::graph.laplacian(g,sparse=FALSE)
+    n <- igraph::vcount(g)
+    A <- L+matrix(1/n,n,n)
+    C <- solve(A)
+    rel <- resistanceDistance(C,n)
+    rel <- FUN(rel,...)
+  }else stop(paste(relation,"is not defined as indirect relation"))
+  return(rel)
 }
