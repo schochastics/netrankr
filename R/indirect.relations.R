c82a600e5640d5249ac6642098228e727c1d9b2f
schochastics <david.schoch@uni-konstanz.de>
2017-05-12 10:26:39 +0200

    added relations and better manual

--- R/indirect.relations.R
+++ R/indirect.relations.R
@@ -1,11 +1,9 @@
 #' @title Indirect relations of a network
-#' @description Derive indirect relations like distances, for a given network. This function
-#' can be used to calculate a variety of centrality indices.
-#' @param g igraph object. The network for which relations should be derived
-#' @param relation string. giving the relation to be calculated. One of "identity", 
-#' "distances", "dependencies" or "walks". See Details.
-#' @param FUN a weighting function. See Details.
-#' @param ... additional arguments passed to the function FUN.
+#' @description Derive indirect relations like distances for a given network. 
+#' @param g igraph object. The network for which relations should be derived.
+#' @param relation string. giving the relation to be calculated. See Details for options.
+#' @param FUN a function that allows the transformation of relations. See Details.
+#' @param ... additional arguments passed to FUN.
 #' @details The \emph{relation} parameter has the following options.  
 #' 
 #' \emph{"identity"} returns the adjacency matrix of the network.  
@@ -15,8 +13,8 @@
 #' \emph{"dependencies"} returns dyadic dependencies 
 #' \deqn{\delta(u,s) = \sum_{t \in V} \frac{\sigma(s,t|u)}{\sigma(s,t)}}
 #' where \eqn{\sigma(s,t|u)} is the number of shortest paths from s to t that include u and
-#' \eqn{\sigma(s,t)} is the total number of shortest (s,t)-paths. The row sums of the resulting matrix 
-#' is equal to the betweenness scores.  
+#' \eqn{\sigma(s,t)} is the total number of shortest (s,t)-paths. This relation is mostly used
+#' for betweenness-like centrality indices.
 #' 
 #' \emph{"walks"} returns walk counts between pairs of nodes, usually they are 
 #' weighted decreasingly in their lengths or other properties which can be done by adding
@@ -25,18 +23,24 @@
 #' \emph{"resistance"} returns the resistance distance between all pairs of vertices.
 #'  
 #' The function \code{FUN} is used to further specify the indirect
-#' relation. see `?transform_relations` for predefined functions.
+#' relation. See [transform_relations] for predefined functions and additional help.
 #' 
 #' @return a matrix containing indirect relations in a network.
 #' @author David Schoch
+#' @seealso [aggregate_index] to build centrality indices, [positional_dominance] to derive dominance relations
 #' @examples
-#' 
 #' require(igraph)
+#' g <- graph.empty(n=11,directed = FALSE)
+#' g <- add_edges(g,c(1,11,2,4,3,5,3,11,4,8,5,9,5,11,6,7,6,8,
+#'                    6,10,6,11,7,9,7,10,7,11,8,9,8,10,9,10))
+#' #geodesic distances
+#' D <- indirect_relations(g,relation="geodesic") 
+#' #dyadic dependencies
+#' D <- indirect_relations(g,relation="dependencies")
 #' 
 #' @export
 indirect_relations <- function(g,relation="geodesic",
                                FUN=identity,...){
-  
   if(relation=="geodesic"){
     rel <- igraph::distances(g,mode = "all")
     rel <- FUN(rel,...)
--- R/transform.relations.R
+++ R/transform.relations.R
@@ -1,43 +1,48 @@
 #' @title transformation of indirect relations 
-#' @description Mostly wrapper functions to transform indirect relations.
+#' @description Mostly wrapper functions that can be used in conjunction 
+#' with [indirect_relations] to fine tune indirect relations.
 #' @param x matrix of transformed relations.
 #' @param alpha weighting factor.
 #' @param k for walk counts up to a certain length.
-#' @description This functions can be used in conjunction with [indirect_relations].
+#' @details The predefined functions follow the naming scheme `relation_transformation`.
+#' Predifined functions `walks_*` are thus best used with relation="walks" in
+#' [indirect_relations]. Theoretically, however, any transformation can be used with any relation.
+#' Yet the results might not be interpretable.
+#' 
+#' The follwing functions are implemented: 
+#' 
+#' `dist_2pow` returns \eqn{2^-x}
+#' 
+#' `dist_inv` returns \eqn{1/x}
+#'  
+#' `dist_dpow` returns \eqn{x^{-\alpha}} where \eqn{\alpha} should be chosen greater than 0.
+#' 
+#' `dist_powd` returns \eqn{\alpha^x} where \eqn{\alpha} should be chosen between 0 and 1.
+#' 
+#' `walks_limit_prop` returns the limit proportion of walks between pairs of nodes. Calculating
+#' rowSums of this relation will result in the principle eigenvector of the network.
+#' 
+#' `walks_exp` returns \eqn{\sum_{k=0}^\infty \frac{A^k}{k!}}
+#' 
+#' `walks_exp_even` returns \eqn{\sum_{k=0}^\infty \frac{A^{2k}}{(2k)!}}
+#' 
+#' `walks_exp_odd` returns \eqn{\sum_{k=0}^\infty \frac{A^{2k+1}}{(2k+1)!}}
+#' 
+#' `walks_attenuated` returns \eqn{\sum_{k=0}^\infty \alpha^k A^k}
+#' 
+#' `walks_uptok` returns \eqn{\sum_{j=0}^k \alpha^j A^j}
+#' 
+#' Walk based transformation are defined on the eigen decomposition of the 
+#' adjacency matrix using the fact that
+#' \deqn{f(A)=Xf(\Lambda)X^T.}
+#' Care has to be taken when using user defined functions. 
+#' 
+#' 
 #' @name transform_relations
 #' @return transformed relations as matrix
 #' @author David Schoch
 NULL
 
-#' @rdname transform_relations
-#' @export
-walks_limit_prop <- function(x){c(1,rep(0,length(x)-1))}
-
-#' @rdname transform_relations
-#' @export
-walks_even_exp <- function(x,alpha=1) {cosh(alpha*x)}
-
-#' @rdname transform_relations
-#' @export
-walks_odd_exp <- function(x,alpha=1) {sinh(alpha*x)}
-
-#' @rdname transform_relations
-#' @export
-walks_exp <- function(x,alpha=1) {exp(alpha*x)}
-
-#' @rdname transform_relations
-#' @export
-walks_attenuated <- function(x,alpha=1/max(x)*0.99) {1/(1-alpha*x)}
-
-#' @rdname transform_relations
-#' @export
-walks_uptok <- function(x,alpha=1,k=3) {
-  y <- 0
-  for(i in 1:k){
-    y <- y+alpha^i*x^i
-  }
-  return(y)
-}
 
 #' @rdname transform_relations
 #' @export
@@ -56,4 +61,39 @@ dist_dpow <- function(x,alpha=1){x^(-alpha)}
 
 #' @rdname transform_relations
 #' @export
-dist_powd <- function(x,alpha=0.5){alpha^(-x)}
+dist_powd <- function(x,alpha=0.5){alpha^(x)}
+
+#' @rdname transform_relations
+#' @export
+walks_limit_prop <- function(x){c(1,rep(0,length(x)-1))}
+
+#' @rdname transform_relations
+#' @export
+walks_exp <- function(x,alpha=1) {exp(alpha*x)}
+
+#' @rdname transform_relations
+#' @export
+walks_exp_even <- function(x,alpha=1) {cosh(alpha*x)}
+
+#' @rdname transform_relations
+#' @export
+walks_exp_odd <- function(x,alpha=1) {sinh(alpha*x)}
+
+#' @rdname transform_relations
+#' @export
+walks_attenuated <- function(x,alpha=1/max(x)*0.99) {
+  if(alpha>1/max(x)){
+    warning(paste0("alpha should be smaller than ",1/max(x),". Results are most likely wrong otherwise."))
+  }
+  1/(1-alpha*x)
+  }
+
+#' @rdname transform_relations
+#' @export
+walks_uptok <- function(x,alpha=1,k=3) {
+  y <- 0
+  for(i in 1:k){
+    y <- y+alpha^i*x^i
+  }
+  return(y)
+}
