71f5e433d62ce06ae2b897328e49abc05958a002
d-mathlete <daschoka@googlemail.com>
2017-09-17 15:17:27 +0200

    simplified input parameter

--- R/get.rankings.R
+++ R/get.rankings.R
@@ -1,40 +1,47 @@
 #' @title All rankings of a partial ranking
 #' @description Returns all possible rankings of a partial ranking.
 #'
-#' @param lattice adjacency list of predecessors in lattice of ideal. This list is returned if [exact_rank_prob] 
-#' is run with `only.results=FALSE`
-#' @param ideals list of ideals. This list is returned if [exact_rank_prob] 
-#' is run with `only.results=FALSE`
-#' @param topo.order topological order used in [exact_rank_prob]
-#' @param linext number of possible rankings. returned by [exact_rank_prob]
-#' @param mse equivalence classes of rankings. returned by [exact_rank_prob]
+#' @param data list as returned by [exact_rank_prob] when run with `only.results=FALSE`
 #' @param force boolean. stops function if the number of rankings is too large.
 #' Only change to TRUE if you know what you are doing
 #' @details The ith row of the matrix contains the rank of node i in all possible rankings
 #' that are in accordance with the partial ranking P.
-#' @return a matrix containing all ranking positions of nodes
+#' @return a matrix containing ranks of nodes in all possible rankings
 #' @author David Schoch
 #' @examples
 #' P <- matrix(c(0,0,1,1,1,0,0,0,1,0,0,0,0,0,1,rep(0,10)),5,5,byrow=TRUE)
 #' P
 #' res <- exact_rank_prob(P,only.results=FALSE)
-#' get_rankings(res$lattice,res$ideals,res$topo.order,res$lin.ext,res$mse)
+#' get_rankings(res)
 #' 
 #' @export
 
-get_rankings <- function(lattice,ideals,topo.order,linext,mse,force=F){
-  if(linext>10000 & !force){
-    stop("number of linear extensions to high. Use force=F if you know what you are doing.")
-  }
-  n <- length(unique(mse))
-  lattice <- lapply(lattice,function(x)x+1)
-  g <- igraph::graph_from_adj_list(lattice,mode="in")
-  paths <- igraph::all_shortest_paths(g,from=n+1,to=1)
-  
-  paths <- lapply(paths$res,function(x) as.vector(x)-1)
-  rks <- rankings(paths, ideals, linext, n)
-  rks <- rks+1
-  rks <- rks[order(topo.order),]
-  rks <- rks[mse,]
-  return(rks)
+get_rankings <- function(data, force = F) {
+    if (missing(data)){
+      stop("no data provided")
+    }
+    if (!all(c("lattice", "ideals", "topo.order", "lin.ext", "mse")%in%names(data))){
+      stop("data is in wrong format. run exact_rank_prob with only.results=F")
+    }
+    lattice <- data$lattice
+    ideals <- data$ideals
+    topo.order <- data$topo.order
+    linext <- data$lin.ext
+    mse <- data$mse
+    
+    if (linext > 50000 & !force) {
+      stop("number of possible rankings is very high. Use force=F if you know what you are doing.")
+    }
+    
+    n <- length(unique(mse))
+    lattice <- lapply(lattice, function(x) x + 1)
+    g <- igraph::graph_from_adj_list(lattice, mode = "in")
+    paths <- igraph::all_shortest_paths(g, from = n + 1, to = 1)
+    
+    paths <- lapply(paths$res, function(x) as.vector(x) - 1)
+    rks <- rankings(paths, ideals, linext, n)
+    rks <- rks + 1
+    rks <- rks[order(topo.order), ]
+    rks <- rks[mse, ]
+    return(rks)
 }
