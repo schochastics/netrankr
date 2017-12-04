e5ac6e2cbb2c185d6b06bbe237012cd49fed5cad
schochastics <david.schoch@uni-konstanz.de>
2017-12-04 18:53:38 +0100

    added linebreak

--- R/indirect.relations.R
+++ R/indirect.relations.R
@@ -68,7 +68,7 @@
 indirect_relations <- function(g, type = "geodesic", 
                                log_param = NULL,FUN = identity, ...) {
     if(type=="dependencies"){
-      warning('"dependencies" is deprecated. Use "depend_sp" instead ')
+      warning('"dependencies" is deprecated. Use "depend_sp" instead.\n')
       type <- "depend_sp"
     }
     if (type == "geodesic") {
