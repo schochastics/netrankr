9da1c0f1b2c2e906773d10db52a0f7aa9e5ce18c
schochastics <david.schoch@uni-konstanz.de>
2017-12-07 09:47:30 +0100

    code restructured (no moar c&p!)

--- R/index.builder.R
+++ R/index.builder.R
@@ -9,33 +9,57 @@ index_builder <- function() {
   if (!requireNamespace("shiny", quietly = TRUE)) {
     stop("shiny is needed for the addin to work. Please install it.", call. = FALSE)
   }
+  dist_transform <- c(identity="identity",`1/x`="dist_inv",`2^-x`="dist_2pow",
+                      `a^x`="dist_powd",`x^-a`="dist_dpow")
+  walk_transform <- c(`limit proportion`="walks_limit_prop",`exponential`="walks_exp",
+                      `even exponential`="walks_exp_even",`odd exponential`="walks_exp_odd",
+                      `attenuated walks`="walks_attenuated",
+                      `up to length k` = "walks_uptok")
+  
   transforms <- list(identity="identity",
-                     dist_sp=c(identity="identity",`1/x`="dist_inv",`2^-x`="dist_2pow",`a^x`="dist_powd",`x^-a`="dist_dpow"),
-                     dist_resist=c(identity="identity",`1/x`="dist_inv",`2^-x`="dist_2pow",`a^x`="dist_powd",`x^-a`="dist_dpow"),
-                     dist_lf=c(identity="identity",`1/x`="dist_inv",`2^-x`="dist_2pow",`a^x`="dist_powd",`x^-a`="dist_dpow"),
+                     dist_sp=dist_transform,
+                     dist_resist=dist_transform,
+                     dist_lf=dist_transform,
                      depend_sp=c(identity="identity"),
                      depend_netflow=c(identity="identity"),
                      depend_curflow=c(identity="identity"),
                      depend_exp=c(identity="identity"),
                      depend_rsps=c(identity="identity"),
                      depend_rspn=c(identity="identity"),
-                     walks=c(`limit proportion`="walks_limit_prop",`exponential`="walks_exp",
-                             `even exponential`="walks_exp_even",`odd exponential`="walks_exp_odd",
-                             `attenuated walks`="walks_attenuated",
-                             `up to length k` = "walks_uptok"))
+                     walks=walk_transform)
   
+  indices <- list("degree"=c("identity","identity","sum"),
+                  "ccclassic"=c("dist_sp","identity","invsum"),
+                  "bcsp"=c("depend_sp","identity","sum"),
+                  "eigen"=c("walks","walks_limit_prop","sum"),
+                  "scall"=c("walks","walks_exp","self"),
+                  "sceven"=c("walks","walks_exp_even","self"),
+                  "scodd"=c("walks","walks_exp_odd","self"),
+                  "katz"=c("walks","walks_attenuated","sum"),
+                  "comall"=c("walks","walks_exp","sum"),
+                  "comeven"=c("walks","walks_exp_even","sum"),
+                  "comodd"=c("walks","walks_exp_odd","sum"),
+                  "netflow"=c("depend_netflow","identity","sum"),
+                  "curflow"=c("depend_curflow","identity","sum"),
+                  "combet"=c("depend_exp","identity","sum"),
+                  "rsps"=c("depend_rsps","identity","sum"),
+                  "rspn"=c("depend_rspn","identity","sum"),
+                  "hcc"=c("dist_sp","dist_inv","sum"),
+                  "rcc"=c("dist_sp","dist_2pow","sum"),
+                  "inf"=c("dist_resist","identity","invsum"),
+                  "gencc"=c("dist_sp","dist_dpow","sum"),
+                  "decay"=c("dist_sp","dist_powd","sum")
+  )
   #ui ----    
   ui <- miniUI::miniPage(
     miniUI::gadgetTitleBar("Centrality Index Builder"),
     shiny::fluidRow(
       shiny::column(6,shiny::selectInput("index", "Prebuild Indices",
-                                         list("Classic Indices"=c("Degree"="degree","Closeness"="ccclassic","Betweenness"="bcsp",
-                                                                  "Eigenvector"="eigen"),
+                                         list("Classic Indices"=c("Degree"="degree","Closeness"="ccclassic",
+                                                                  "Betweenness"="bcsp", "Eigenvector"="eigen"),
                                               "Feedback"=c("Subgraph"="scall","Subgraph even"="sceven",
-                                                           "Subgraph odd"="scodd",
-                                                           "Katz Status"="katz",
-                                                           "Communicability"="comall",
-                                                           "Communcability even"="comeven",
+                                                           "Subgraph odd"="scodd", "Katz Status"="katz",
+                                                           "Communicability"="comall","Communcability even"="comeven",
                                                            "Communcability odd"="comodd"),
                                               "Betweenness Type"=c("Flow Betweenness"="netflow",
                                                                    "Current Flow Betweenness"="curflow",
@@ -63,7 +87,9 @@ index_builder <- function() {
                                                                 "Exponential Walks"="depend_exp",
                                                                 "Simple RSP" = "depend_rsps",
                                                                 "Net RSP" = "depend_rspn")))),
+      
       shiny::column(3, shiny::selectInput("transformation", "Transformation",c(""))),
+      
       shiny::column(3,shiny::selectInput("aggregation", "Aggregation",
                                          c("Sum"="sum","Mean"="mean","Max"="max","Min"="min",
                                            "Inverse Sum"="invsum","Self"="self",
@@ -128,95 +154,15 @@ index_builder <- function() {
     
     shiny::observe({
       shiny::updateSelectInput(session, "transformation", 
-                        label = "Transformation", 
-                        choices = transforms[[input$relation]])
+                               label = "Transformation", 
+                               choices = transforms[[input$relation]])
     })
     shiny::observe({
-      if(input$index=='degree'){
-        shiny::updateSelectInput(session,"relation",selected="identity")
-        shiny::updateSelectInput(session,"transformation",selected="identity")
-        shiny::updateSelectInput(session,"aggregation",selected="sum")
-      } else if(input$index=="ccclassic"){
-        shiny::updateSelectInput(session,"relation",selected="dist_sp")
-        shiny::updateSelectInput(session,"transformation",selected="identity")
-        shiny::updateSelectInput(session,"aggregation",selected="invsum")
-      } else if(input$index=="bcsp"){
-        shiny::updateSelectInput(session,"relation",selected="depend_sp")
-        shiny::updateSelectInput(session,"transformation",selected="identity")
-        shiny::updateSelectInput(session,"aggregation",selected="sum")
-      } else if(input$index=="eigen"){
-        shiny::updateSelectInput(session,"relation",selected="walks")
-        shiny::updateSelectInput(session,"transformation",selected="walks_limit_prop")
-        shiny::updateSelectInput(session,"aggregation",selected="sum")
-      } else if(input$index=="scall"){
-        shiny::updateSelectInput(session,"relation",selected="walks")
-        shiny::updateSelectInput(session,"transformation",selected="walks_exp")
-        shiny::updateSelectInput(session,"aggregation",selected="self")
-      } else if(input$index=="sceven"){
-        shiny::updateSelectInput(session,"relation",selected="walks")
-        shiny::updateSelectInput(session,"transformation",selected="walks_exp_even")
-        shiny::updateSelectInput(session,"aggregation",selected="self")
-      } else if(input$index=="scodd"){
-        shiny::updateSelectInput(session,"relation",selected="walks")
-        shiny::updateSelectInput(session,"transformation",selected="walks_exp_odd")
-        shiny::updateSelectInput(session,"aggregation",selected="self")
-      }else if(input$index=="katz"){
-        shiny::updateSelectInput(session,"relation",selected="walks")
-        shiny::updateSelectInput(session,"transformation",selected="walks_attenuated")
-        shiny::updateSelectInput(session,"aggregation",selected="sum")
-      } else if(input$index=="comall"){
-        shiny::updateSelectInput(session,"relation",selected="walks")
-        shiny::updateSelectInput(session,"transformation",selected="walks_exp")
-        shiny::updateSelectInput(session,"aggregation",selected="sum")
-      } else if(input$index=="comeven"){
-        shiny::updateSelectInput(session,"relation",selected="walks")
-        shiny::updateSelectInput(session,"transformation",selected="walks_exp_even")
-        shiny::updateSelectInput(session,"aggregation",selected="sum")
-      } else if(input$index=="comodd"){
-        shiny::updateSelectInput(session,"relation",selected="walks")
-        shiny::updateSelectInput(session,"transformation",selected="walks_exp_odd")
-        shiny::updateSelectInput(session,"aggregation",selected="sum")
-      } else if(input$index=="netflow"){
-        shiny::updateSelectInput(session,"relation",selected="depend_netflow")
-        shiny::updateSelectInput(session,"transformation",selected="identity")
-        shiny::updateSelectInput(session,"aggregation",selected="sum")
-      } else if(input$index=="curflow"){
-        shiny::updateSelectInput(session,"relation",selected="depend_curflow")
-        shiny::updateSelectInput(session,"transformation",selected="identity")
-        shiny::updateSelectInput(session,"aggregation",selected="sum")
-      } else if(input$index=="combet"){
-        shiny::updateSelectInput(session,"relation",selected="depend_exp")
-        shiny::updateSelectInput(session,"transformation",selected="identity")
-        shiny::updateSelectInput(session,"aggregation",selected="sum")
-      } else if(input$index=="rsps"){
-        shiny::updateSelectInput(session,"relation",selected="depend_rsps")
-        shiny::updateSelectInput(session,"transformation",selected="identity")
-        shiny::updateSelectInput(session,"aggregation",selected="sum")
-      } else if(input$index=="rspn"){
-        shiny::updateSelectInput(session,"relation",selected="depend_rspn")
-        shiny::updateSelectInput(session,"transformation",selected="identity")
-        shiny::updateSelectInput(session,"aggregation",selected="sum")
-      } else if(input$index=="hcc"){
-        shiny::updateSelectInput(session,"relation",selected="dist_sp")
-        shiny::updateSelectInput(session,"transformation",selected="dist_inv")
-        shiny::updateSelectInput(session,"aggregation",selected="sum")
-      } else if(input$index=="rcc"){
-        shiny::updateSelectInput(session,"relation",selected="dist_sp")
-        shiny::updateSelectInput(session,"transformation",selected="dist_2pow")
-        shiny::updateSelectInput(session,"aggregation",selected="sum")
-      } else if(input$index=="gencc"){
-        shiny::updateSelectInput(session,"relation",selected="dist_sp")
-        shiny::updateSelectInput(session,"transformation",selected="dist_dpow")
-        shiny::updateSelectInput(session,"aggregation",selected="sum")
-      } else if(input$index=="decay"){
-        shiny::updateSelectInput(session,"relation",selected="dist_sp")
-        shiny::updateSelectInput(session,"transformation",selected="dist_powd")
-        shiny::updateSelectInput(session,"aggregation",selected="sum")
-      }  else if(input$index=="inf"){
-        shiny::updateSelectInput(session,"relation",selected="dist_resist")
-        shiny::updateSelectInput(session,"transformation",selected="identity")
-        shiny::updateSelectInput(session,"aggregation",selected="invsum")
-      }
+      index_focus <- indices[[input$index]]
+      shiny::updateSelectInput(session,"relation",selected=index_focus[1])
+      shiny::updateSelectInput(session,"transformation",selected=index_focus[2])
+      shiny::updateSelectInput(session,"aggregation",selected=index_focus[3])
+      
     })
     shiny::observeEvent(input$done, {
       lfparam_text <- ifelse(input$relation!="dist_lf","",paste0(", lfparam = ",input$lfparam))
