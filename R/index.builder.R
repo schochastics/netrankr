a6a98e3b5e9454d2e5ab75b74964a3c38999b67e
schochastics <david.schoch@uni-konstanz.de>
2017-12-13 10:23:09 +0100

    function typo fixed

--- R/index.builder.R
+++ R/index.builder.R
@@ -183,13 +183,13 @@ index_builder <- function() {
                           "type = \"", 
                           input$relation,"\"",lfparam_text,netflow_text,rspx_text,
                           ", FUN = ",input$transformation,alpha_text,tok_text,") %>%\n\t",
-                          "aggregate_position(type = \"",input$aggregation,"\")")
+                          "aggregate_positions(type = \"",input$aggregation,"\")")
       } else {
         indexText <- paste0("rel <- indirect_relations(g,",
                             "type = \"", 
                             input$relation,"\"",lfparam_text,netflow_text,rspx_text,
                             ", FUN = ",input$transformation,alpha_text,tok_text,")\n",
-                            "cent <- aggregate_position(rel,type = \"",input$aggregation,"\")")
+                            "cent <- aggregate_positions(rel,type = \"",input$aggregation,"\")")
       }
       rstudioapi::insertText(indexText)
       shiny::stopApp()
