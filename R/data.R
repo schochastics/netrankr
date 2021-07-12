e0fc61299128ad9438d557c5896c5298cae33d0c
schochastics <david@schochastics.net>
2021-07-12 14:56:11 +0100

    added dbces11 graph

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' dbces11 graph
+#' @description Smallest graph (11 nodes and 17 edges) where the centers according to (d)egree, (b)etweenness, (c)loseness, (e)igenvector centrality, and (s)ubgraph centrality are all different.
+#'
+#' @format igraph object
+"dbces11"
+
+
+#' Florentine family marriage network
+#'
+#' @format An igraph object containing marriage links of florentine families
+#' 
+#' @references Padgett, J.F. and Ansell, C.K., 1993. 
+#' Robust Action and the Rise of the Medici, 1400-1434. *American Journal of Sociology*, **98**(6), 1259-1319.
+"florentine_m"
