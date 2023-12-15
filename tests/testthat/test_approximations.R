3c9c43178f2084ca8b46dac4b2c0ce714ec6a6be
schochastics <david@schochastics.net>
2023-12-15 06:56:04 +0100

    more tests

--- tests/testthat/test_approximations.R
+++ tests/testthat/test_approximations.R
@@ -4,41 +4,54 @@ library(magrittr)
 library(Matrix)
 
 test_that("approximate expected is correct", {
-  P <- matrix(c(0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, rep(0, 10)), 5, 5, byrow = TRUE)
-  methods_str <- c("lpom", "glpom", "loof1", "loof2")
-  res <- round(sapply(methods_str, function(x) approx_rank_expected(P, method = x)), 3)
-  lpom <- c(1.2, 2.0, 3.0, 4.5, 4.5)
-  glpom <- c(1.250, 2.167, 2.833, 4.333, 4.417)
-  loof1 <- c(1.333, 2.143, 2.917, 4.250, 4.357)
-  loof2 <- c(1.340, 2.204, 2.910, 4.198, 4.349)
+    P <- matrix(c(0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, rep(0, 10)), 5, 5, byrow = TRUE)
+    methods_str <- c("lpom", "glpom", "loof1", "loof2")
+    res <- round(sapply(methods_str, function(x) approx_rank_expected(P, method = x)), 3)
+    lpom <- c(1.2, 2.0, 3.0, 4.5, 4.5)
+    glpom <- c(1.250, 2.167, 2.833, 4.333, 4.417)
+    loof1 <- c(1.333, 2.143, 2.917, 4.250, 4.357)
+    loof2 <- c(1.340, 2.204, 2.910, 4.198, 4.349)
 
 
-  expect_equal(res[, 1], lpom)
-  expect_equal(res[, 2], glpom)
-  expect_equal(res[, 3], loof1)
-  expect_equal(res[, 4], loof2)
+    expect_equal(res[, 1], lpom)
+    expect_equal(res[, 2], glpom)
+    expect_equal(res[, 3], loof1)
+    expect_equal(res[, 4], loof2)
 
-  expect_error(approx_rank_expected(1))
-  expect_error(approx_rank_expected(matrix(c(0, 2, 2, 0), 2, 2)))
+    expect_error(approx_rank_expected(1))
+    expect_error(approx_rank_expected(matrix(c(0, 2, 2, 0), 2, 2)))
 })
 
 test_that("approximate relative is correct", {
-  P <- matrix(c(0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, rep(0, 10)), 5, 5, byrow = TRUE)
-  rel_noiter <- approx_rank_relative(P, iterative = FALSE)
-  rel_iter1 <- approx_rank_relative(P, iterative = TRUE, num.iter = 1)
+    P <- matrix(c(0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, rep(0, 10)), 5, 5, byrow = TRUE)
+    rel_noiter <- approx_rank_relative(P, iterative = FALSE)
+    rel_iter1 <- approx_rank_relative(P, iterative = TRUE, num.iter = 1)
 
-  expect_equal(rel_noiter, rel_iter1)
+    expect_equal(rel_noiter, rel_iter1)
 
-  P <- matrix(1, 10, 10)
-  rownames(P) <- colnames(P) <- paste0("V", 1:10)
-  P[lower.tri(P, diag = T)] <- 0
-  rel_iter10 <- approx_rank_relative(P, iterative = TRUE, num.iter = 10)
-  rownames(rel_iter10) <- colnames(rel_iter10) <- paste0("V", 1:10)
-  rel_mcmc <- mcmc_rank_prob(P, rp = 100)$relative.rank
-  expect_equal(P, rel_iter10)
-  expect_equal(P, rel_mcmc)
+    P <- matrix(1, 10, 10)
+    rownames(P) <- colnames(P) <- paste0("V", 1:10)
+    P[lower.tri(P, diag = T)] <- 0
+    rel_iter10 <- approx_rank_relative(P, iterative = TRUE, num.iter = 10)
+    rownames(rel_iter10) <- colnames(rel_iter10) <- paste0("V", 1:10)
+    rel_mcmc <- mcmc_rank_prob(P, rp = 100)$relative.rank
+    expect_equal(P, rel_iter10)
+    expect_equal(P, rel_mcmc)
 
-  P <- matrix(1, 5, 5) - diag(1, 5)
-  expect_error(approx_rank_expected(P))
-  expect_error(approx_rank_relative(P))
+    P <- matrix(1, 5, 5) - diag(1, 5)
+    expect_error(approx_rank_expected(P))
+    expect_error(approx_rank_relative(P))
+
+    P <- matrix(1, 10, 10)
+    P[lower.tri(P, diag = T)] <- 0
+    rel_iter10 <- approx_rank_relative(P, iterative = TRUE, num.iter = 10)
+    rel_mcmc <- mcmc_rank_prob(P, rp = 100)$relative.rank
+    rownames(rel_mcmc) <- colnames(rel_mcmc) <- NULL
+
+    expect_equal(P, rel_iter10)
+    expect_true(all(P == rel_mcmc))
+})
+
+test_that("mcmc error andling works", {
+    expect_error(mcmc_rank_prob("a"))
 })
--- tests/testthat/test_dominance.R
+++ tests/testthat/test_dominance.R
@@ -3,68 +3,70 @@ library(igraph)
 library(magrittr)
 library(Matrix)
 test_that("neighborhood-inclusion is correct", {
-  data("dbces11")
-  A <- matrix(0, 11, 11)
-  rownames(A) <- colnames(A) <- LETTERS[1:11]
-  A[c(23, 35, 45, 47, 56, 67, 79, 111, 113)] <- 1
+    data("dbces11")
+    A <- matrix(0, 11, 11)
+    rownames(A) <- colnames(A) <- LETTERS[1:11]
+    A[c(23, 35, 45, 47, 56, 67, 79, 111, 113)] <- 1
 
-  expect_equal(neighborhood_inclusion(dbces11, sparse = FALSE), A)
+    expect_equal(neighborhood_inclusion(dbces11, sparse = FALSE), A)
 
-  ####
+    ####
 
-  tg <- threshold_graph(10, 0)
-  V(tg)$name <- LETTERS[1:10]
-  A <- matrix(1, 10, 10)
-  rownames(A) <- colnames(A) <- LETTERS[1:10]
-  diag(A) <- 0
-  A[10, ] <- 0
+    tg <- threshold_graph(10, 0)
+    V(tg)$name <- LETTERS[1:10]
+    A <- matrix(1, 10, 10)
+    rownames(A) <- colnames(A) <- LETTERS[1:10]
+    diag(A) <- 0
+    A[10, ] <- 0
 
-  expect_equal(neighborhood_inclusion(tg, sparse = FALSE), A)
+    expect_equal(neighborhood_inclusion(tg, sparse = FALSE), A)
 })
 
 test_that("positional dominance is correct", {
-  g <- graph.empty(n = 11, directed = FALSE)
-  g <- add_edges(g, c(
-    1, 11, 2, 4, 3, 5, 3, 11, 4, 8, 5, 9, 5, 11, 6, 7, 6, 8,
-    6, 10, 6, 11, 7, 9, 7, 10, 7, 11, 8, 9, 8, 10, 9, 10
-  ))
+    g <- graph.empty(n = 11, directed = FALSE)
+    g <- add_edges(g, c(
+        1, 11, 2, 4, 3, 5, 3, 11, 4, 8, 5, 9, 5, 11, 6, 7, 6, 8,
+        6, 10, 6, 11, 7, 9, 7, 10, 7, 11, 8, 9, 8, 10, 9, 10
+    ))
 
-  D <- g %>%
-    indirect_relations() %>%
-    positional_dominance(map = FALSE, benefit = FALSE)
+    D <- g %>%
+        indirect_relations() %>%
+        positional_dominance(map = FALSE, benefit = FALSE)
 
-  Dmap <- g %>%
-    indirect_relations() %>%
-    positional_dominance(map = TRUE, benefit = FALSE)
+    Dmap <- g %>%
+        indirect_relations() %>%
+        positional_dominance(map = TRUE, benefit = FALSE)
 
-  A <- matrix(0, 11, 11)
-  A[c(23, 35, 45, 47, 56, 67, 79, 111, 113)] <- 1
-  Amap <- matrix(0, 11, 11)
-  Amap[c(
-    2, 23, 24, 34, 35, 45, 46, 47, 48, 56, 57, 58, 59, 60,
-    62, 63, 64, 65, 67, 68, 69, 70, 71, 78, 79, 80, 81, 82,
-    84, 86, 87, 89, 90, 91, 92, 93, 95, 96, 98, 100, 101, 102,
-    103, 111, 112, 113, 114, 115, 117
-  )] <- 1
+    A <- matrix(0, 11, 11)
+    A[c(23, 35, 45, 47, 56, 67, 79, 111, 113)] <- 1
+    Amap <- matrix(0, 11, 11)
+    Amap[c(
+        2, 23, 24, 34, 35, 45, 46, 47, 48, 56, 57, 58, 59, 60,
+        62, 63, 64, 65, 67, 68, 69, 70, 71, 78, 79, 80, 81, 82,
+        84, 86, 87, 89, 90, 91, 92, 93, 95, 96, 98, 100, 101, 102,
+        103, 111, 112, 113, 114, 115, 117
+    )] <- 1
 
-  expect_equal(D, A)
-  expect_equal(Dmap, Amap)
+    expect_equal(D, A)
+    expect_equal(Dmap, Amap)
 
-  Data_mat <- matrix(c(10:1, 1:10), 10, 2)
-  D <- positional_dominance(Data_mat, type = "two-mode")
-  expect_equal(sum(abs(D)), 0)
+    Data_mat <- matrix(c(10:1, 1:10), 10, 2)
+    D <- positional_dominance(Data_mat, type = "two-mode")
+    expect_equal(sum(abs(D)), 0)
+
+    expect_error(positional_dominance("a"))
 })
 
 test_that("dominance_graph is correct", {
-  library(igraph)
-  library(magrittr)
+    library(igraph)
+    library(magrittr)
 
-  g <- graph.empty(n = 11, directed = FALSE)
-  g <- add_edges(g, c(
-    1, 11, 2, 4, 3, 5, 3, 11, 4, 8, 5, 9, 5, 11, 6, 7, 6, 8,
-    6, 10, 6, 11, 7, 9, 7, 10, 7, 11, 8, 9, 8, 10, 9, 10
-  ))
-  P <- neighborhood_inclusion(g, sparse = FALSE)
-  g_dom <- dominance_graph(P)
-  expect_equal(sum(P), ecount(g_dom))
+    g <- graph.empty(n = 11, directed = FALSE)
+    g <- add_edges(g, c(
+        1, 11, 2, 4, 3, 5, 3, 11, 4, 8, 5, 9, 5, 11, 6, 7, 6, 8,
+        6, 10, 6, 11, 7, 9, 7, 10, 7, 11, 8, 9, 8, 10, 9, 10
+    ))
+    P <- neighborhood_inclusion(g, sparse = FALSE)
+    g_dom <- dominance_graph(P)
+    expect_equal(sum(P), ecount(g_dom))
 })
